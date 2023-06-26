da7c9de248d9db314449a86510f8b756e6f0e551
junyanzh <ny8426@icloud.com>
2023-06-26 07:36:46 +0000

    新增兩個股票的漲跌幅比較功能

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def compare_stocks():
     data = request.get_json()
     ticker1 = data['ticker1']
     ticker2 = data['ticker2']
+    ticker3 = data['ticker3']  # 新增的第三個股票
     
     end = datetime.today().strftime('%Y-%m-%d')
     start = (datetime.today() - timedelta(days=10)).strftime('%Y-%m-%d')
@@ -16,16 +17,24 @@ def compare_stocks():
     try:
         ticker1_data = yf.download(ticker1, start=start, end=end)
         ticker2_data = yf.download(ticker2, start=start, end=end)
+        ticker3_data = yf.download(ticker3, start=start, end=end)  # 新增的第三個股票的資料
     except Exception as e:
         return jsonify({'error': f'Could not retrieve ticker: {e}'})
 
     ticker1_change = (ticker1_data['Close'][-1] - ticker1_data['Close'][0]) / ticker1_data['Close'][0]
     ticker2_change = (ticker2_data['Close'][-1] - ticker2_data['Close'][0]) / ticker2_data['Close'][0]
+    ticker3_change = (ticker3_data['Close'][-1] - ticker3_data['Close'][0]) / ticker3_data['Close'][0]  # 新增的第三個股票的變化
     
-    if ticker1_change > ticker2_change:
-        return jsonify({'ticker': ticker1, 'change': ticker1_change})
-    else:
-        return jsonify({'ticker': ticker2, 'change': ticker2_change})
+    # 用來比較三個股票的變化
+    changes = {
+        'ticker1': ticker1_change,
+        'ticker2': ticker2_change,
+        'ticker3': ticker3_change,
+    }
+    
+    max_ticker = max(changes, key=changes.get)
+    
+    return jsonify({'ticker': max_ticker, 'change': changes[max_ticker]})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
